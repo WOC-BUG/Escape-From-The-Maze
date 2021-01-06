29291c6e43b7ff2af19984d364d1a6fc36d4eb5c
WOC_BUG <1539777243@qq.com>
2021-01-06 22:55:47 +0800

    添加组件、UI、场景切换

---
+++ EscapeFromTheMaze/Assets/Scenes/Scripts/Collide.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Collide : MonoBehaviour
+{
+    static public int number = 0;
+    void OnTriggerEnter(Collider other)
+    {
+        number++;
+        Debug.Log(number);
+        SceneManager.LoadScene("Vuforia"); //切换场景
+    }
+}
