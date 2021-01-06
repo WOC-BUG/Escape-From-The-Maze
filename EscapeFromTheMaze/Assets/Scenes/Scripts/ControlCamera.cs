ca628c9829504de28d9d1f1ed27f23c28f6375f8
WOC_BUG <1539777243@qq.com>
2021-01-06 19:42:56 +0800

    创建地形

---
+++ EscapeFromTheMaze/Assets/Scenes/Scripts/ControlCamera.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlCamera : MonoBehaviour
+{
+    public Transform SphereTransform;   //设置一个距离的三维变量，用于保存摄像机于小球之间的距离
+    private Vector3 distance;
+
+    // Use this for initialization
+    void Start()
+    {
+        //transform.position为主摄像机的位置
+        //SphereTransform.position为小球的位置
+        distance = transform.position - SphereTransform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //主摄像机的位置为每次小球移动的位置+之间的距离
+        transform.position = SphereTransform.position + distance;
+    }
+}
---
+++ EscapeFromTheMaze/Assets/Scenes/Scripts/Run.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Run : MonoBehaviour
+{    
+    private Rigidbody rb;   //刚体对象    
+    public int speed = 5;   //小球移动速度
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>(); //接收刚体组件
+    }
+
+    void Update()
+    {
+        //Input.GetAxis用于检测是否按下键盘按键
+        float h = Input.GetAxis("Horizontal");  //Horizontal表示水平按键
+        float v = Input.GetAxis("Vertical");    //Vertical表示垂直按键
+        
+        rb.AddForce(new Vector3(h, 0, v) * speed);  //添加一个力
+    }
+}
