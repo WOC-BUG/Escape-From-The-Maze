dad8cf394ba9954691080d4a249358cbfb967cab
WOC_BUG <1539777243@qq.com>
2021-01-07 17:43:12 +0800

    获取物体

---
+++ EscapeFromTheMaze/Assets/Scenes/Scripts/CollideSnow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CollideSnow : MonoBehaviour
+{
+    public GameObject parameter;
+    void Start()
+    {
+        GameObject.DontDestroyOnLoad(parameter);
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        parameter.GetComponent<PassParameter>().number++;  //获取物品数目+1
+        parameter.GetComponent<PassParameter>().snow = true;    // 标记获得雪花
+        Debug.Log("是否获取到星星：" + parameter.GetComponent<PassParameter>().star);
+        Debug.Log("是否获取到礼物：" + parameter.GetComponent<PassParameter>().present);
+        Debug.Log("是否获取到雪花：" + parameter.GetComponent<PassParameter>().snow);
+        //加载场景，但不销毁原场景
+        //LoadSceneMode有两种：Single表示关闭原场景，Additive表示将场景添加到当前加载的场景中
+        SceneManager.LoadSceneAsync("Vuforia", LoadSceneMode.Single);
+    }
+}
---
+++ EscapeFromTheMaze/Assets/Scenes/Scripts/PresentRotate.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PresentRotate : MonoBehaviour
+{
+    private GameObject pm;
+    public GameObject present;
+    public float speed = 30.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pm = GameObject.Find("Parameter");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (pm.GetComponent<PassParameter>().present == false)
+            present.SetActive(false);//隐藏物体
+        else
+            present.SetActive(true);//显示物体
+        transform.Rotate(Vector3.down * Time.deltaTime * speed);
+    }
+}
